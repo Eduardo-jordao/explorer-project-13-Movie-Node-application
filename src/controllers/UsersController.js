1e0456e20c34bc6c542c2927791534144bbe8fcd
Eduardo <eduardojdecastro@gmail.com>
2023-12-14 00:13:32 -0300

    Integração Backend e Frontend 1

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -25,7 +25,7 @@ class UsersController {
 
   async update(request, response) {
      const { name, email, password, old_password } = request.body
-     const { id } = request.params
+     const user_id = request.user.id;
 
      const database = await sqliteConnection()
      const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
